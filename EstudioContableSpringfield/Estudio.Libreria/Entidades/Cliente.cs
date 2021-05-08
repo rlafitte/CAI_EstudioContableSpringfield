9073d2b17329f6ca7905493ba5298925e4a237c7
Renard <rdlafitte@gmail.com>
2021-05-08 12:47:51 -0300

    Cliente y Persona

---
+++ EstudioContableSpringfield/Estudio.Libreria/Entidades/Cliente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudio.Libreria.Entidades
+{
+    public class Cliente : Persona
+    {
+        private int _id;
+
+        public int Id { get => _id; set => _id = value; }
+
+        public Cliente(int id, Persona p)
+        {
+            this.Id = id;
+            this.Nombre = p.Nombre;
+            this.Apellido = p.Apellido;
+            this.TipoDocumento = p.TipoDocumento;
+            this.Documento = p.Documento;
+        }
+    }
+}
---
+++ EstudioContableSpringfield/Estudio.Libreria/Entidades/Persona.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudio.Libreria.Entidades
+{
+    public abstract class Persona
+    {
+        private string _nombre;
+        private string _apellido;
+        private int _tipoDocumento;
+        private int _documento;
+
+        public string Nombre { get => _nombre; set => _nombre = value; }
+        public string Apellido { get => _apellido; set => _apellido = value; }
+        public int TipoDocumento { get => _tipoDocumento; set => _tipoDocumento = value; }
+        public int Documento { get => _documento; set => _documento = value; }
+        public Persona(string s1, string s2, int i1, int i2)
+        {
+            this.Nombre = s1;
+            this.Apellido = s2;
+            this.TipoDocumento = i1;
+            this.Documento = i2;
+        }
+        public Persona()
+        {
+
+        }
+
+        public virtual string ToStringPersona()
+        {
+            return $"Apellido: { this.Apellido}, Nombre: {this.Nombre}, Tipo y N° de documento:{this.TipoDocumento} - {this.Documento}";
+        }
+    }
+}
