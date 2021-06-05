5063c9a212062ba3164592c9386a70e23f60cbc4
Renard <rdlafitte@gmail.com>
2021-06-05 09:40:51 -0300

    datacontract

--- EstudioContableSpringfield/Estudio.Libreria/Entidades/Liquidacion.cs
+++ EstudioContableSpringfield/Estudio.Libreria/Entidades/Liquidacion.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Estudio.Libreria.Entidades
 {
+    [DataContract]
     public class Liquidacion
     {
         private string _codLiquidacion;
@@ -21,16 +23,18 @@ namespace Estudio.Libreria.Entidades
         private double _bruto;
         private double _retenciones;
         private double _neto;
-
+        [DataMember(Name = "apellido")]
         public string CodLiquidacion { get => this._codLiquidacion; }
         public Empresa Empresa { get => this._empresa; }
         public Empleado Empleado { get => this._empleado; }
         public Categoria Categoria { get => this._categoria; }
+        [DataMember(Name ="Periodo")]
+        public int Mes { get => _mes; set => _mes = value; }
 
         public Liquidacion(string codLiquidacion, int mes, int año, string tipo, DateTime fechaPago, Empresa empresa, Empleado empleado)
         {
             this._codLiquidacion = codLiquidacion;
-            this._mes = mes;
+            this.Mes = mes;
             this._año = año;
             this._tipo = tipo;
             this._fechaPago = fechaPago;
@@ -47,7 +51,7 @@ namespace Estudio.Libreria.Entidades
 
         public override string ToString()
         {
-            return $"Código: {this._codLiquidacion} | Fecha: {this._mes}/{this._año} | Tipo: {this._tipo} | Empresa: {this._empresa} | Empleado: {this._empleado} | Categoria: {this._categoria}";
+            return $"Código: {this._codLiquidacion} | Fecha: {this.Mes}/{this._año} | Tipo: {this._tipo} | Empresa: {this._empresa} | Empleado: {this._empleado} | Categoria: {this._categoria}";
         }
     }
 }
